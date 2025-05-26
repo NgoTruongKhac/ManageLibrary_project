c280a7c858022cc8ca81718523111c7466861ebf
minh <22130171@st.hcmuaf.edu.vn>
2025-05-27 00:40:06 +0700

    searchBook

---
+++ client/src/components/booksSearched.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState, useRef } from "react";
+import apiBook from "../api/book.api";
+import { Link } from "react-router-dom";
+
+export default function BooksSearched({ onKeyWord, onClose }) {
+  const [results, setResults] = useState([]);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    console.log("search");
+    const delayDebounce = setTimeout(() => {
+      if (onKeyWord.trim() !== "") {
+        apiBook
+          .getBooksByTitle(onKeyWord)
+          .then((res) => setResults(res.data.books || []))
+          .catch((err) => {
+            console.error("Lỗi tìm kiếm sách:", err);
+            setResults([]);
+          });
+      } else {
+        setResults([]);
+      }
+    }, 300); // debounce 300ms
+
+    return () => clearTimeout(delayDebounce);
+  }, [onKeyWord]);
+
+  // Xử lý click ngoài dropdown để ẩn kết quả
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setResults([]); // Ẩn dropdown
+        if (onClose) onClose(); // Nếu cần báo cho cha component
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
+  if (onKeyWord.trim() === "") return null;
+
+  if (results.length === 0)
+    return (
+      <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-[80%] mt-2 bg-white shadow-lg z-20 rounded-lg p-2 max-h-[300px] overflow-y-auto border">
+        không có sách phù hợp
+      </div>
+    );
+
+  return (
+    <div
+      ref={containerRef}
+      className="dropdown-content absolute top-full left-1/2 transform -translate-x-1/2 w-[80%] mt-2 bg-white shadow-lg z-20 rounded-lg p-2 max-h-[392px] overflow-y-auto border"
+    >
+      {results.map((book) => (
+        <Link
+          to={`/${book.category}/${book.subCategory}/${book._id}`}
+          key={book._id}
+          className="flex items-center gap-2 p-1 hover:bg-gray-100 rounded-md"
+        >
+          <img
+            src={book.coverImage}
+            alt={book.title}
+            className="w-14 object-cover rounded-md"
+          />
+          <span className="font-medium text-gray-700 truncate block max-w-full text-sm">
+            {book.title}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+}
